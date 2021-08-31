3cb46b0291391bbaa1ea4c34b0391411594a16c1
badaslko <65233193+badaslko@users.noreply.github.com>
2021-08-31 00:01:48 -0300

    Update play.js

--- comandos/play.js
+++ comandos/play.js
@@ -5,16 +5,16 @@ module.exports = {
     name: 'play',
     description: 'Entra na call e da play em um video do youtube',
     async execute(message, args) {
-        const { joinVoiceChannel } = require('@discord.js/voice');
-        client.on('messageCreate', message => {
-            if(message.content === '!join') {
-                joinVoiceChannel({
-                    channelId: message.member.voice.channel.id,
-                    guildId: message.guild.id,
-                    adapterCreator: message.guild.voiceAdapterCreator
-                })
-            }
+const { joinVoiceChannel } = require('@discord.js/voice');
+client.on('messageCreate', message => {
+    if(message.content === '!join') {
+        joinVoiceChannel({
+            channelId: message.member.voice.channel.id,
+            guildId: message.guild.id,
+            adapterCreator: message.guild.voiceAdapterCreator
         })
+    }
+})
 
         const voiceChannel = message.member.voice.channel;
  
