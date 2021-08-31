7e050e8e06aee8859bb58e8ebb857cc3a54b3751
badaslko <65233193+badaslko@users.noreply.github.com>
2021-08-30 23:55:12 -0300

    voice

--- comandos/play.js
+++ comandos/play.js
@@ -5,16 +5,7 @@ module.exports = {
     name: 'play',
     description: 'Entra na call e da play em um video do youtube',
     async execute(message, args) {
-        const { voiceChannel } = require('@discord.js/voice');
-    client.on('messageCreate', message => {
-    if(message.content === '!join') {
-        voiceChannel({
-            channelId: message.member.voice.channel.id,
-            guildId: message.guild.id,
-            adapterCreator: message.guild.voiceAdapterCreator
-        })
-    }
-})
+        const voiceChannel = message.member.voice.channel;
  
         if (!voiceChannel) return message.channel.send('Você precisa estar em um canal de voz para usar este comando!');
         const permissions = voiceChannel.permissionsFor(message.client.user);
