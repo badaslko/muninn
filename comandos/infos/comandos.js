8ef96c5bc8776475d5b5c15df402c12bd57cc6a6
badaslko <65233193+badaslko@users.noreply.github.com>
2021-08-31 11:38:16 -0300

    a

--- comandos/infos/comandos.js
+++ comandos/infos/comandos.js
@@ -7,18 +7,6 @@ module.exports = {
     
 
     execute(client, message, args){
-        message.channel.send({
-            embed: {
-                color: '#E6E6FA',
-                author: { name: 'Comandos' },
-                footer: { text: 'Bot feito por バダス' },
-                fields: [
-                    { name: 'help', value: 'Usando *help (comando) você ve as informações do comando', inline: true},
-                    { name: 'debug', value: 'mostra quantos canais o bot está conectado', inline: true},
-                ],
-                timestamp: new Date(),
-                description: `To use filters, ${client.config.discord.prefix}filter (the filter). Example : ${client.config.discord.prefix}filter 8D.`,
-            },
-        });
-    }
+        message.channel.send({embc});
+}
 }
--- index.js
+++ index.js
@@ -35,19 +35,14 @@ for (const file of player) {
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 
-const emb = new Discord.MessageEmbed()
-    .setColor('#ff8c00')
-    .setTitle('Classes')
+const embc = new Discord.MessageEmbed()
+    .setColor('#E6E6FA')
+    .setTitle('Comandos')
     .setDescription('Comandos do server')
     .addFields(
-        { name: '<:Assassin_Icon:790354713211174932> Assassin', value: 'Usa sua agilidade e forca para dar um grande dano no inimigo ardilosamente'},
-        { name: '<:Fighter_Icon:790354726595985409> Fighter', value: 'Geralmente a linha de frente junto com o tanker, utiliza sua forca bruta para causar dano'},
-        { name: '<:Healer_Icon:790354690683437127> Healer', value: 'Uma das Classes mais importantes, Utiliza sua magia para curar ou buffar o time, e tambem pode se camuflar'},
-        { name: '<:JEAN_Mage_Icon:790354700712411146> Mage', value: 'Um ser humano com habilidades magicas que pode lancar raios, criar lancas de fogo ou entao uma parede de gelo'},
-        { name: '<:Ranger_Icon:790355408769384448> Ranger', value: ' Um arqueiro habil, usa seu arco para dar dano, buffar o time ou reduzir a defesa do inimigo'},
-        { name: '<:Tanker_Icon:790354674929631282> Tanker', value: 'Uma das Classes necessarias, Usa sua defesa descomunal para puxar o aggro de todos os inimigos'},
+        { name: 'teste', value: 'teste'},
     )
-    .setFooter('Utilize g!<nome da classe> para ver suas skills');
+    .setFooter('Utilize *h <nome do comando> para ver o que ele faz');
     
 client.once('ready', () => {
     console.log('Muninn esta expandindo sua mente!');
