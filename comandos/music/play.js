3ffbb6853848cee50e5706927af89c9d50033e49
badaslko <65233193+badaslko@users.noreply.github.com>
2021-08-31 04:44:15 -0300

    Update play.js

--- comandos/music/play.js
+++ comandos/music/play.js
@@ -10,6 +10,19 @@ module.exports = {
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - Você não está no mesmo canal de voz que o bot!`);
 
         if (!args[0]) return message.channel.send(`${client.emotes.error} - Por favor, indique a musica que você quer tocar!`);
+        function play(connection, message){
+            var server = servers[message.guild.id];
+        
+            server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: 
+        "audioonly"}));
+        
+            server.queue.shift();
+        
+            server.dispatcher.on("end", function() {
+                if(server.queue[0]) play(connection, message);
+                else connection.disconnect();
+            });
+        }
 
         client.player.play(message, args.join(" "), { firstResult: true });
     },
