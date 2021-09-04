639d7e5cf6febf73634d087746507e9b86e25061
badaslko <65233193+badaslko@users.noreply.github.com>
2021-09-04 02:29:51 -0300

    Create play.js

---
+++ comandos/music/play.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name: 'play',
+    aliases: ['p'],
+    category: 'Music',
+    utilisation: '{prefix}play [name/URL]',
+
+    execute(client, message, args) {
+        if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - Voce não está em um canal de voz`);
+
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(`${client.emotes.error} - You are not in the same voice channel !`);
+
+        if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+    let guildQueue = client.player.getQueue(message.guild.id);
+
+    if(command === 'play') {
+        let queue = client.player.createQueue(message.guild.id);
+        await queue.join(message.member.voice.channel);
+        let song = await queue.play(args.join(' ')).catch(_ => {
+            if(!guildQueue)
+                queue.stop();
+        });
+    }
+    },
+};
