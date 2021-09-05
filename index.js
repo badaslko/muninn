1c85fdf1917705d4279efb70f90feddfbf173033
badaslko <65233193+badaslko@users.noreply.github.com>
2021-09-05 02:02:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ client.on('messageCreate', async (message) => {
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
     if(command=== 'teste'){
-        message.channel.send(embt)
+        message.channel.send({ embeds: [embt] })
     }else if(command === 'play' || 'p') {        
         let queue = client.player.createQueue(message.guild.id);        
         await queue.join(message.member.voice.channel);       
